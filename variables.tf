2ec99a53a48035a4139a6bbca84416d46242b8b0
Sasha1371 <91017528+Sasha1371@users.noreply.github.com>
2023-10-21 12:35:52 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,3 @@
-variable "accses_key" {
-  type = string
-  default = "AKIAWLMF4K7JOY5NCAHY"
-}
-
-variable "secret_key" {
-  type = string
-  default = "0mqDZszEVZKKkKRJDj30A0sHsO7RdCHptIr9MixE"
-}
 variable "aws_instance_type" {
   type = string
   default = "t3.micro"
