2304929bd98b8a3e35ed91b1bf77db62a553e9d7
Sasha1371 <91017528+Sasha1371@users.noreply.github.com>
2023-10-21 11:30:55 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "accses_key" {
+  type = string
+  default = "AKIAWLMF4K7JOY5NCAHY"
+}
+
+variable "secret_key" {
+  type = string
+  default = "0mqDZszEVZKKkKRJDj30A0sHsO7RdCHptIr9MixE"
+}
+variable "aws_instance_type" {
+  type = string
+  default = "t3.micro"
+}
+variable "aws_ami" {
+  type = string
+  default = "ami-0989fb15ce71ba39e"
+}
